57d3d5f7c57e6647c71d6e0732ce087af5cdd5d2
Ashish <ashishraj108786@gmail.com>
2018-02-06 16:22:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashishrajput/notification/notification.java
@@ -0,0 +1,17 @@
+package com.example.ashishrajput.notification;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ashish Rajput on 6/18/2017.
+ */
+
+public class notification extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
